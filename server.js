96920780ccf59c25ead6c946582e9bd863556d2f
Utkarsh Saxena <utksxn96@gmail.com>
2020-07-14 19:04:00 +0530

    Final commit

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ var app = express();
 const timeRouter = require('./routes/routes');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-//COMMENTING THESE 2 LINES OF CODE AS A TEST
-//var cors = require('cors');
-//app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+//so that my API is remotely testable
+var cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
